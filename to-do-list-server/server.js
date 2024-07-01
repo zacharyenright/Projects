0cd045afda276a38300ba19fe342a077af02cd57
zacharyenright <70986414+zacharyenright@users.noreply.github.com>
2024-07-01 18:39:04 +0000

    changed comments

--- to-do-list-server/server.js
+++ to-do-list-server/server.js
@@ -10,21 +10,20 @@ const api = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appi
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Weather API route
+// Weather API 
 app.get("/weather", (req, res) => {
   https.get(api, (response) => {
     let data = '';
 
-    // A chunk of data has been received.
+    
     response.on("data", (chunk) => {
       data += chunk;
     });
 
-    // The whole response has been received.
     response.on("end", () => {
       const weatherData = JSON.parse(data);
       console.log(weatherData);
-      res.json(weatherData); // Sending the weather data as the response
+      res.json(weatherData); 
     });
   }).on("error", (err) => {
     console.log("Error: " + err.message);
